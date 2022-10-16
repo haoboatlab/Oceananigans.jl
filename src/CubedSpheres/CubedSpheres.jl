e3dc2e55521f675f350a681f958d0a208d4cd976
Simone Silvestri <silvestri.simone0@gmail.com>
2022-10-15 23:25:31 -0400

    bugfic

--- src/CubedSpheres/CubedSpheres.jl
+++ src/CubedSpheres/CubedSpheres.jl
@@ -140,7 +140,7 @@ end
 ##### Don't splite kernels for the cubed sphere!
 #####
 
-import Oceanananigans.TimeStepper: validate_kernel_splitting
+import Oceananigans.TimeStepper: validate_kernel_splitting
 
 @inline validate_kernel_splitting(grid::ConformalCubedSphereFaceGrid) = false
 
