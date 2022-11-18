e3f9502d7ce176bd2b760d43dff31f70a857349c
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-17 23:25:26 -0500

    more tests

--- src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_ab2_step.jl
+++ src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_ab2_step.jl
@@ -60,7 +60,7 @@ function ab2_step_velocities!(velocities, model, Δt, χ)
 
         push!(explicit_velocity_step_events, event)
     end
-
+    
     wait(device(architecture(model.grid)), MultiEvent(tuple(explicit_velocity_step_events...)))
 
     for (i, name) in enumerate((:u, :v))
