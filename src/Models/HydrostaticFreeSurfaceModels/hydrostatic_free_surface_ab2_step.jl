64039d99480eb024e7caea96742c480466efdf0e
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-17 23:16:44 -0500

    small bugfix

--- src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_ab2_step.jl
+++ src/Models/HydrostaticFreeSurfaceModels/hydrostatic_free_surface_ab2_step.jl
@@ -61,7 +61,7 @@ function ab2_step_velocities!(velocities, model, Δt, χ)
         push!(explicit_velocity_step_events, event)
     end
 
-    wait(device(architecture(model.grid)), MultiEvent(Tuple(explicit_velocity_step_events...)))
+    wait(device(architecture(model.grid)), MultiEvent(tuple(explicit_velocity_step_events...)))
 
     for (i, name) in enumerate((:u, :v))
         velocity_field = model.velocities[name]
