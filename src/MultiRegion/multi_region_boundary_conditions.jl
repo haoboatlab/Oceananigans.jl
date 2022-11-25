9686561ff5e4346b8e2146d970a257de0b2ed335
Simone Silvestri <silvestri.simone0@gmail.com>
2022-11-25 14:15:41 -0500

    first commit

--- src/MultiRegion/multi_region_boundary_conditions.jl
+++ src/MultiRegion/multi_region_boundary_conditions.jl
@@ -272,7 +272,7 @@ end
 @inline getregion(bc::BoundaryCondition, i) = BoundaryCondition(bc.classification, _getregion(bc.condition, i))
 
 @inline getregion(cf::ContinuousBoundaryFunction{X, Y, Z, I}, i) where {X, Y, Z, I} =
-    ContinuousBoundaryFunction{X, Y, Z, I}(cf.func::F,
+    ContinuousBoundaryFunction{X, Y, Z, I}(cf.func,
                                            _getregion(cf.parameters, i),
                                            cf.field_dependencies,
                                            cf.field_dependencies_indices,
@@ -283,25 +283,25 @@ end
 
 
 @inline _getregion(fc::FieldBoundaryConditions, i) = 
-FieldBoundaryConditions(getregion(fc.west, i), 
-                        getregion(fc.east, i), 
-                        getregion(fc.south, i), 
-                        getregion(fc.north, i), 
-                        getregion(fc.bottom, i),
-                        getregion(fc.top, i),
-                        fc.immersed)
+    FieldBoundaryConditions(getregion(fc.west, i), 
+                            getregion(fc.east, i), 
+                            getregion(fc.south, i), 
+                            getregion(fc.north, i), 
+                            getregion(fc.bottom, i),
+                            getregion(fc.top, i),
+                            fc.immersed)
 
 @inline _getregion(bc::BoundaryCondition, i) = BoundaryCondition(bc.classification, getregion(bc.condition, i))
 
 @inline _getregion(cf::ContinuousBoundaryFunction{X, Y, Z, I}, i) where {X, Y, Z, I} =
-ContinuousBoundaryFunction{X, Y, Z, I}(cf.func::F,
-                                   getregion(cf.parameters, i),
-                                   cf.field_dependencies,
-                                   cf.field_dependencies_indices,
-                                   cf.field_dependencies_interp)
+    ContinuousBoundaryFunction{X, Y, Z, I}(cf.func,
+                                           getregion(cf.parameters, i),
+                                           cf.field_dependencies,
+                                           cf.field_dependencies_indices,
+                                           cf.field_dependencies_interp)
 
 @inline _getregion(df::DiscreteBoundaryFunction, i) =
-DiscreteBoundaryFunction(df.func, getregion(df.parameters, i))
+    DiscreteBoundaryFunction(df.func, getregion(df.parameters, i))
 
 # Everything goes for multi-region BC
 validate_boundary_condition_location(::MultiRegionObject, ::Center, side)       = nothing 
